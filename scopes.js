e96e63fee09f16ff6eb3a417c888659014769ffb
Pravesh Niroula <Pravesh31.745421@pmc.edu.np>
2022-03-28 21:50:39 +0545

    scope haru

---
+++ scopes.js
@@ -0,0 +1,22 @@
+// var globalScope = 10;
+
+
+// if (true) {
+//     var insideIf = 20;
+// }
+
+
+// console.log(insideIf);
+
+let globalScope = 58;
+
+for (var i = 0; i < 2; i++) {
+    // console.log(globalScope)
+    var localScope = 30;
+    // console.log(localScope)
+}
+
+
+// console.log(globalScope);
+console.log(localScope);
+
